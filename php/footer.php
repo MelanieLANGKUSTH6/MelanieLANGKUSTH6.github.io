5692cd69d5383ae695d7f268e3c897cbb34e326f
MelanieLANGKUSTH6 <melanie.langkusth@oclock.school>
2023-07-05 09:57:33 +0200

    Test sur première section + ajout de l'étiquette en_construction

---
+++ php/footer.php
@@ -0,0 +1,21 @@
+<footer>
+        <nav class="footer__menu">
+            <ul class="footer__menu-ul">
+                <li class="footer__menu-li">A propos</li>
+                <li class="footer__menu-li">Compétences</li>
+                <li class="footer__menu-li">Expériences [&] Formation</li>
+                <li><img class="footer__img" src="" alt=""></li>
+                <li class="footer__menu-li">Portfolio</li>
+                <li class="footer__menu-li">Outils / Compétences</li>
+                <li class="footer__menu-li">Contacts</li>
+            </ul>
+        </nav>
+        <div class="footer__reseaux">
+            <a href=""><img class="footer__reseaux__img" src="" alt=""></a>
+            <a href=""><img class="footer__reseaux__img" src="" alt=""></a>
+        </div>
+        <p class="footer__texte"> Mélanie LANGKUSTH &#169; 2023 <br> Tous droits réservés</p>
+    </footer>
+    <script src="../js/animation.js"></script>
+</body>
+</html>
